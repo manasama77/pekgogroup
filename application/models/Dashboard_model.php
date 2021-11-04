61fb52c3ba371c3d6488a3dbcef9911e5b9238f7
Adam PM <adam.pm77@gmail.com>
2021-11-04 09:04:54 +0700

    upd dashboard

--- application/models/Dashboard_model.php
+++ application/models/Dashboard_model.php
@@ -8,6 +8,8 @@ class Dashboard_model extends CI_Model
     public function get_total_order()
     {
         $this->db->where('month(created_at)', date('m'));
+        $this->db->where('status', 'active');
+        $this->db->where('deleted_at', null);
         $exec = $this->db->get('orders');
         return $exec->num_rows();
     }
